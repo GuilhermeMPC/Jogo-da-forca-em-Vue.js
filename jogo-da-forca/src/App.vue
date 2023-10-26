f2d4fa8d68b6ccd7d01ce25733d2a5a559f0fcb2
Guilherme Midea Paoliello Castilho <2200770@SALAS.AULAS>
2023-10-25 21:57:36 -0300

    ajsutes

--- jogo-da-forca/src/App.vue
+++ jogo-da-forca/src/App.vue
@@ -1,16 +1,37 @@
 <template>
   <div>
-    <h1>Jogo da Forca</h1>
-    <img :src="`assets/forca-${hangmanStage}.png`" alt="Estágio da Forca" />
-    <WordToGuess :word="wordToGuess" :guessedWord="guessedWord" />
-    <AlphabetButtons :disabledLetters="guessedLetters" @guess="makeGuess" />
-    <div v-if="gameOver">
-      <p v-if="playerWon">Você venceu!</p>
-      <p v-else>Você perdeu. A palavra era: {{ wordToGuess }}</p>
-      <button @click="resetGame">Jogar novamente</button>
+    <div id="header">
+      <h1 id="title">Jogo da Forca</h1>
+    </div>
+    <div id="game-area">
+      <div class="left-column">
+        <img :src="require(`@/assets/forca-${hangmanStage}.png`)" alt="Estágio da Forca" />
+      </div>
+      <div class="right-column">
+        <div id="word-container">
+          <WordToGuess :word="wordToGuess" :guessedWord="guessedWord" />
+        </div>
+        <div id="button-container">
+          <div class="button-row">
+            <AlphabetButtons :disabledLetters="guessedLetters" @guess="makeGuess" :letters="['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']" />
+          </div>
+          <div class="button-row">
+            <AlphabetButtons :disabledLetters="guessedLetters" @guess="makeGuess" :letters="['I', 'J', 'K', 'L', 'M', 'N', 'O', 'P']" />
+          </div>
+          <div class="button-row">
+            <AlphabetButtons :disabledLetters="guessedLetters" @guess="makeGuess" :letters="['Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']" />
+          </div>
+        </div>
+        <div v-if="gameOver" id="game-over">
+          <p v-if="playerWon">Você venceu!</p>
+          <p v-else>Você perdeu. A palavra era: {{ wordToGuess }}</p>
+          <button @click="resetGame">Jogar novamente</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
+
 
 <script>
 import WordToGuess from './components/WordToGuess.vue';
@@ -71,5 +92,105 @@ export default {
 </script>
 
 <style>
-/* Estilos para o componente App */
+body {
+  font-family: Arial, sans-serif;
+  background-color: #f7f7f7;
+  margin: 0;
+  padding: 0;
+}
+
+.container {
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+}
+
+/* Estilos do cabeçalho */
+#header {
+  background-color: #333;
+  color: #fff;
+  text-align: center;
+  padding: 20px 0;
+}
+
+#title {
+  font-size: 36px;
+  margin: 0;
+}
+
+/* Estilos do conteúdo do jogo */
+#game-area {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 20px;
+}
+
+.left-column {
+  flex: 1;
+  max-width: 50%;
+  text-align: center;
+}
+
+.right-column {
+  flex: 1;
+  max-width: 50%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+/* Estilos da imagem da forca */
+img {
+  max-width: 100%;
+  height: auto;
+}
+
+/* Estilos do contêiner da palavra a ser adivinhada */
+#word-container {
+  text-align: center;
+}
+
+/* Estilos dos botões do alfabeto */
+.button-row {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  background-color: #0074D9;
+  color: #fff;
+  padding: 10px 20px;
+  font-size: 16px;
+  margin: 5px;
+  border: none;
+  cursor: pointer;
+}
+
+button:hover {
+  background-color: #0056b3;
+}
+
+/* Estilos da seção de game over */
+#game-over {
+  text-align: center;
+  margin-top: 20px;
+}
+
+#game-over p {
+  font-size: 18px;
+  margin: 10px 0;
+}
+
+/* Estilos gerais para dispositivos menores (responsivo) */
+@media (max-width: 768px) {
+  #game-area {
+    flex-direction: column;
+  }
+
+  .left-column, .right-column {
+    max-width: 100%;
+  }
+}
 </style>
